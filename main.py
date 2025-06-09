34ca5b2e1e3b64ce9691b307748277e2e640da6b
SethK99 <sgkloster@gmail.com>
2025-06-09 14:29:04 -0700

    use types.Content for prompt input

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 # main.py
 
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
-import sys
+from google.genai import types
 
 def main():
+    # Check for CLI argument
     if len(sys.argv) < 2:
         print("Error: Please provide a prompt as a command-line argument.")
         sys.exit(1)
-    prompt = " ".join(sys.argv[1:])
+
+    user_prompt = " ".join(sys.argv[1:])
 
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
@@ -18,15 +21,17 @@ def main():
 
     client = genai.Client(api_key=api_key)
 
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)])
+    ]
+
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
-        contents=prompt
+        contents=messages
     )
 
-    
     print(response.text)
 
-    
     usage = response.usage_metadata
     print(f"Prompt tokens: {usage.prompt_token_count}")
     print(f"Response tokens: {usage.candidates_token_count}")
